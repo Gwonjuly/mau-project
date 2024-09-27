13571bb3cca8b07fa1f42832e7c400a306755ce6
Gwonjuly <rnjswnfl99@gmail.com>
2024-09-27 21:25:36 +0900

    init

---
+++ mau-project/src/main/java/com/mau/mau_project/MauProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mau.mau_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MauProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MauProjectApplication.class, args);
+	}
+
+}
