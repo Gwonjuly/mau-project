23d52dab517a107588d7bec39006538bea2eff63
Gwonjuly <rnjswnfl99@gmail.com>
2024-10-06 16:41:51 +0900

    User setting 오류 해결: Repository Bean 등록

--- mau-project/src/main/java/com/mau/mau_project/MauProjectApplication.java
+++ mau-project/src/main/java/com/mau/mau_project/MauProjectApplication.java
@@ -2,14 +2,8 @@ package com.mau.mau_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.mau.mau-project.entity")
-@EnableJpaRepositories(basePackages = "com.mau.mau-project.repository")
-@ComponentScan(basePackages = "com.mau.mau-project")
 public class MauProjectApplication {
 
 	public static void main(String[] args) {
